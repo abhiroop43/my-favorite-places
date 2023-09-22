5f2a2e12179c7b59c1d436acce75a7fd64a0a9a7
Abhiroop Santra <abhiroop.santra@gmail.com>
2023-09-22 18:41:41 +0400

    added places list and add places form

---
+++ constants/colors.js
@@ -0,0 +1,13 @@
+export const Colors = {
+  primary50: "#E3F2FD",
+  primary100: "#BBDEFB",
+  primary200: "#90CAF9",
+  primary300: "#64B5F6",
+  primary400: "#42A5F5",
+  primary500: "#2196F3",
+  primary600: "#1E88E5",
+  primary700: "#1976D2",
+  primary800: "#1565C0",
+  accent500: "#FF5733",
+  grey700: "#333333",
+};
