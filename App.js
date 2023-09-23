5eb4b603fc9c33984b84a7badf6f0c6ee98d58a6
Abhiroop Santra <abhiroop.santra@gmail.com>
2023-09-23 11:35:24 +0400

    completed locate user and pick on map

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import AllPlaces from "./screens/AllPlaces";
 import AddPlace from "./screens/AddPlace";
 import IconButton from "./components/UI/IconButton";
 import { Colors } from "./constants/colors";
+import Map from "./screens/Map";
 
 const Stack = createNativeStackNavigator();
 export default function App() {
@@ -39,6 +40,7 @@ export default function App() {
             component={AddPlace}
             options={{ title: "Add A New Place" }}
           />
+          <Stack.Screen name="Map" component={Map} />
         </Stack.Navigator>
       </NavigationContainer>
     </>
